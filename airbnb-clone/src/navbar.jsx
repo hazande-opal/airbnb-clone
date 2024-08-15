355f690d14325ef598d267708ac2f8def47a2b84
hazandeopal <hazandeaustine@gmail.com>
2024-08-15 11:22:19 +0300

    Created the navbar.

--- airbnb-clone/src/navbar.jsx
+++ airbnb-clone/src/navbar.jsx
@@ -7,9 +7,11 @@ function Navbar () {
                 <button className="experience-button">Experience</button>
             </div>
             <div className="right-section">
-                <button>Airbnb your home?</button>
-                <div>
-                    <span><img src="../src/assets/icons8-menu-50.png" alt="" /></span>
+                <button className="airbnb-home">Airbnb your home</button>
+                <span className="favicon-container"><img className="favicon-icon" src="../src/assets/icons8-favicon-50.png" alt="" /></span>
+                <div className="user-logo">
+                    <span className="menu-container"><img className="menu-icon" src="../src/assets/icons8-menu-50.png" alt="" /></span>
+                    <span className="person-logo-container"><img className="person-icon" src="../src/assets/icons8-person-64.png" alt="" /></span>
                 </div>
             </div>
         </nav>
